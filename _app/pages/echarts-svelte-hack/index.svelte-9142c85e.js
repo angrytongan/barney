9909b6e7a923b6dda220ee4498633a0f6051e50f
Dean Fogarty <angrytongan@gmail.com>
2021-11-27 12:12:48 +1000

    Add mount rushmore points test.

---
+++ _app/pages/echarts-svelte-hack/index.svelte-9142c85e.js
@@ -0,0 +1,79 @@
+import{S as He,i as Je,s as Ke,G as We,e as h,j as Be,k as I,c as _,a as P,m as Ze,n as N,d as l,f as $,o as Oe,D as t,u as ne,w as je,x as Z,v as Qe,t as o,g as m,b as d,P as C,Q as U,I as D,h as le,M as xe,R as et,N as tt,r as Re,T as ye}from"../../chunks/vendor-279e1e6e.js";import{G as it,a as at,b as ut,c as st,d as nt,e as lt}from"../../chunks/GraphScatterMultiSeriesSerially-e0ef9d5f.js";import{c as Te}from"../../chunks/store-7f595076.js";function Xe(u,g,a){const c=u.slice();return c[8]=g[a],c[10]=a,c}function Ye(u){let g,a,c,f;var b=u[8];function q(s){return{props:{t:s[1],numPoints:s[2].numPoints,numSeries:s[2].numSeries,height:ot,id:"graph-"+s[10]}}}return b&&(a=new b(q(u))),{c(){g=h("div"),a&&Be(a.$$.fragment),c=I()},l(s){g=_(s,"DIV",{});var S=P(g);a&&Ze(a.$$.fragment,S),c=N(S),S.forEach(l)},m(s,S){$(s,g,S),a&&Oe(a,g,null),t(g,c),f=!0},p(s,S){const k={};if(S&2&&(k.t=s[1]),S&4&&(k.numPoints=s[2].numPoints),S&4&&(k.numSeries=s[2].numSeries),b!==(b=s[8])){if(a){Re();const V=a;ne(V.$$.fragment,1,0,()=>{Qe(V,1)}),je()}b?(a=new b(q(s)),Be(a.$$.fragment),Z(a.$$.fragment,1),Oe(a,g,c)):a=null}else b&&a.$set(k)},i(s){f||(a&&Z(a.$$.fragment,s),f=!0)},o(s){a&&ne(a.$$.fragment,s),f=!1},d(s){s&&l(g),a&&Qe(a)}}}function rt(u){let g=!1,a=()=>{g=!1},c,f,b,q,s,S,k,V,B,G,re,oe,x,p,M,me,O=u[1].toFixed(2)+"",ee,ce,A,pe,j=u[2].numPoints+"",te,de,y,fe,Q=u[2].numSeries+"",ie,ve,ae,ue,v,R,ge,X,he,_e,Y,qe,z,be,Se,H,Pe,Ee,J,ke,K,we,se,w,F,Me,Ie;We(u[4]);let L=u[3],n=[];for(let e=0;e<L.length;e+=1)n[e]=Ye(Xe(u,L,e));const ze=e=>ne(n[e],1,1,()=>{n[e]=null});return{c(){f=h("p"),b=o("A quick hack combining "),q=h("a"),s=o("Svelte's"),S=I(),k=h("a"),V=o(`Block-level
+        element binding`),B=o(" with "),G=h("a"),re=o("Apache ECharts"),oe=o("."),x=I(),p=h("div"),M=h("input"),me=o(" t = "),ee=o(O),ce=I(),A=h("input"),pe=o(" numPoints = "),te=o(j),de=I(),y=h("input"),fe=o(" numSeries = "),ie=o(Q),ve=o(`
+
+    y = `),ae=o(u[0]),ue=I(),v=h("div"),R=h("p"),ge=o(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Pharetra magna ac
+        placerat vestibulum lectus. Nunc sed velit dignissim sodales ut. Malesuada
+        pellentesque elit eget gravida cum sociis. Feugiat pretium nibh ipsum
+        consequat nisl. Nulla facilisi etiam dignissim diam quis. Elementum
+        curabitur vitae nunc sed velit.`),X=h("p"),he=o(`Sem viverra aliquet eget sit amet. Mauris
+        vitae ultricies leo integer malesuada nunc vel. Volutpat lacus laoreet non
+        curabitur gravida arcu ac tortor dignissim. Laoreet non curabitur gravida
+        arcu ac tortor dignissim convallis. Volutpat lacus laoreet non curabitur
+        gravida. Fermentum leo vel orci porta non pulvinar neque laoreet
+        suspendisse.`),_e=I(),Y=h("p"),qe=o(`Amet risus nullam eget felis. Ipsum consequat nisl vel pretium lectus
+        quam. Massa tempor nec feugiat nisl. Porta lorem mollis aliquam ut
+        porttitor leo. Nisi porta lorem mollis aliquam ut porttitor leo a diam.
+        Tristique risus nec feugiat in fermentum posuere urna nec. Massa
+        placerat duis ultricies lacus sed turpis tincidunt id. Odio eu feugiat
+        pretium nibh ipsum consequat nisl vel pretium. Sit amet est placerat in
+        egestas erat. Aliquam sem et tortor consequat. Sed vulputate odio ut
+        enim blandit volutpat maecenas.`),z=h("p"),be=o(`Sit amet purus gravida quis blandit
+        turpis cursus in hac. Cursus metus aliquam eleifend mi in nulla posuere
+        sollicitudin. Iaculis urna id volutpat lacus laoreet non curabitur
+        gravida arcu. Tristique nulla aliquet enim tortor at auctor urna nunc.
+        Rutrum tellus pellentesque eu tincidunt tortor aliquam. Arcu odio ut
+        sem nulla. Semper quis lectus nulla at volutpat diam.`),Se=I(),H=h("p"),Pe=o(`Eu consequat ac felis donec et. Lobortis mattis aliquam faucibus purus
+        in massa tempor. Ultricies integer quis auctor elit sed vulputate.
+        Feugiat in fermentum posuere urna nec tincidunt praesent semper
+        feugiat. Et netus et malesuada fames ac turpis. Congue quisque egestas
+        diam in. Dolor sit amet consectetur adipiscing elit ut. Eget dolor
+        morbi non arcu risus quis varius. Sit amet cursus sit amet dictum.
+        Vitae elementum curabitur vitae nunc sed velit dignissim sodales.`),Ee=I(),J=h("p"),ke=o(`Scelerisque eleifend donec pretium vulputate sapien nec. Commodo sed
+        egestas egestas fringilla phasellus faucibus scelerisque eleifend
+        donec. Ut tristique et egestas quis ipsum suspendisse ultrices.
+        Accumsan tortor posuere ac ut consequat semper. Nunc id cursus metus
+        aliquam eleifend mi in nulla posuere.`),K=h("p"),we=o(`A diam sollicitudin tempor id eu.
+        Varius morbi enim nunc faucibus a pellentesque sit. Quis lectus nulla
+        at volutpat diam ut venenatis tellus. Feugiat sed lectus vestibulum
+        mattis ullamcorper. Morbi tincidunt augue interdum velit euismod in
+        pellentesque massa.`),se=I(),w=h("div");for(let e=0;e<n.length;e+=1)n[e].c();this.h()},l(e){f=_(e,"P",{});var i=P(f);b=m(i,"A quick hack combining "),q=_(i,"A",{target:!0,href:!0});var r=P(q);s=m(r,"Svelte's"),r.forEach(l),S=N(i),k=_(i,"A",{href:!0});var W=P(k);V=m(W,`Block-level
+        element binding`),W.forEach(l),B=m(i," with "),G=_(i,"A",{target:!0,href:!0});var Ne=P(G);re=m(Ne,"Apache ECharts"),Ne.forEach(l),oe=m(i,"."),i.forEach(l),x=N(e),p=_(e,"DIV",{id:!0,class:!0});var E=P(p);M=_(E,"INPUT",{type:!0,min:!0,max:!0,step:!0}),me=m(E," t = "),ee=m(E,O),ce=N(E),A=_(E,"INPUT",{type:!0,min:!0,max:!0,step:!0}),pe=m(E," numPoints = "),te=m(E,j),de=N(E),y=_(E,"INPUT",{type:!0,min:!0,max:!0,step:!0}),fe=m(E," numSeries = "),ie=m(E,Q),ve=m(E,`
+
+    y = `),ae=m(E,u[0]),E.forEach(l),ue=N(e),v=_(e,"DIV",{style:!0});var T=P(v);R=_(T,"P",{});var Ve=P(R);ge=m(Ve,`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Pharetra magna ac
+        placerat vestibulum lectus. Nunc sed velit dignissim sodales ut. Malesuada
+        pellentesque elit eget gravida cum sociis. Feugiat pretium nibh ipsum
+        consequat nisl. Nulla facilisi etiam dignissim diam quis. Elementum
+        curabitur vitae nunc sed velit.`),Ve.forEach(l),X=_(T,"P",{});var Fe=P(X);he=m(Fe,`Sem viverra aliquet eget sit amet. Mauris
+        vitae ultricies leo integer malesuada nunc vel. Volutpat lacus laoreet non
+        curabitur gravida arcu ac tortor dignissim. Laoreet non curabitur gravida
+        arcu ac tortor dignissim convallis. Volutpat lacus laoreet non curabitur
+        gravida. Fermentum leo vel orci porta non pulvinar neque laoreet
+        suspendisse.`),Fe.forEach(l),_e=N(T),Y=_(T,"P",{});var $e=P(Y);qe=m($e,`Amet risus nullam eget felis. Ipsum consequat nisl vel pretium lectus
+        quam. Massa tempor nec feugiat nisl. Porta lorem mollis aliquam ut
+        porttitor leo. Nisi porta lorem mollis aliquam ut porttitor leo a diam.
+        Tristique risus nec feugiat in fermentum posuere urna nec. Massa
+        placerat duis ultricies lacus sed turpis tincidunt id. Odio eu feugiat
+        pretium nibh ipsum consequat nisl vel pretium. Sit amet est placerat in
+        egestas erat. Aliquam sem et tortor consequat. Sed vulputate odio ut
+        enim blandit volutpat maecenas.`),$e.forEach(l),z=_(T,"P",{});var Ce=P(z);be=m(Ce,`Sit amet purus gravida quis blandit
+        turpis cursus in hac. Cursus metus aliquam eleifend mi in nulla posuere
+        sollicitudin. Iaculis urna id volutpat lacus laoreet non curabitur
+        gravida arcu. Tristique nulla aliquet enim tortor at auctor urna nunc.
+        Rutrum tellus pellentesque eu tincidunt tortor aliquam. Arcu odio ut
+        sem nulla. Semper quis lectus nulla at volutpat diam.`),Ce.forEach(l),Se=N(T),H=_(T,"P",{});var Ge=P(H);Pe=m(Ge,`Eu consequat ac felis donec et. Lobortis mattis aliquam faucibus purus
+        in massa tempor. Ultricies integer quis auctor elit sed vulputate.
+        Feugiat in fermentum posuere urna nec tincidunt praesent semper
+        feugiat. Et netus et malesuada fames ac turpis. Congue quisque egestas
+        diam in. Dolor sit amet consectetur adipiscing elit ut. Eget dolor
+        morbi non arcu risus quis varius. Sit amet cursus sit amet dictum.
+        Vitae elementum curabitur vitae nunc sed velit dignissim sodales.`),Ge.forEach(l),Ee=N(T),J=_(T,"P",{});var Le=P(J);ke=m(Le,`Scelerisque eleifend donec pretium vulputate sapien nec. Commodo sed
+        egestas egestas fringilla phasellus faucibus scelerisque eleifend
+        donec. Ut tristique et egestas quis ipsum suspendisse ultrices.
+        Accumsan tortor posuere ac ut consequat semper. Nunc id cursus metus
+        aliquam eleifend mi in nulla posuere.`),Le.forEach(l),K=_(T,"P",{});var De=P(K);we=m(De,`A diam sollicitudin tempor id eu.
+        Varius morbi enim nunc faucibus a pellentesque sit. Quis lectus nulla
+        at volutpat diam ut venenatis tellus. Feugiat sed lectus vestibulum
+        mattis ullamcorper. Morbi tincidunt augue interdum velit euismod in
+        pellentesque massa.`),De.forEach(l),T.forEach(l),se=N(e),w=_(e,"DIV",{id:!0,style:!0});var Ue=P(w);for(let Ae=0;Ae<n.length;Ae+=1)n[Ae].l(Ue);Ue.forEach(l),this.h()},h(){d(q,"target","_blank"),d(q,"href","https://svelte.dev"),d(k,"href","https://svelte.dev/docs#Block-level_element_bindings"),d(G,"target","_blank"),d(G,"href","https://echarts.apache.org"),d(M,"type","range"),d(M,"min","0.01"),d(M,"max","1"),d(M,"step","0.01"),d(A,"type","range"),d(A,"min","50"),d(A,"max","200"),d(A,"step","10"),d(y,"type","range"),d(y,"min","1"),d(y,"max","5"),d(y,"step","1"),d(p,"id","controls"),d(p,"class","sticky svelte-1cv5vdn"),C(v,"position","relative"),C(v,"top","150px"),C(v,"width","150px"),C(v,"left","80%"),d(w,"id","graphs"),C(w,"position","fixed"),C(w,"top","150px"),C(w,"width","75%"),C(w,"height","50px")},m(e,i){$(e,f,i),t(f,b),t(f,q),t(q,s),t(f,S),t(f,k),t(k,V),t(f,B),t(f,G),t(G,re),t(f,oe),$(e,x,i),$(e,p,i),t(p,M),U(M,u[1]),t(p,me),t(p,ee),t(p,ce),t(p,A),U(A,u[2].numPoints),t(p,pe),t(p,te),t(p,de),t(p,y),U(y,u[2].numSeries),t(p,fe),t(p,ie),t(p,ve),t(p,ae),$(e,ue,i),$(e,v,i),t(v,R),t(R,ge),t(v,X),t(X,he),t(v,_e),t(v,Y),t(Y,qe),t(v,z),t(z,be),t(v,Se),t(v,H),t(H,Pe),t(v,Ee),t(v,J),t(J,ke),t(v,K),t(K,we),$(e,se,i),$(e,w,i);for(let r=0;r<n.length;r+=1)n[r].m(w,null);F=!0,Me||(Ie=[D(window,"scroll",()=>{g=!0,clearTimeout(c),c=setTimeout(a,100),u[4]()}),D(M,"change",u[5]),D(M,"input",u[5]),D(A,"change",u[6]),D(A,"input",u[6]),D(y,"change",u[7]),D(y,"input",u[7])],Me=!0)},p(e,[i]){if(i&1&&!g&&(g=!0,clearTimeout(c),scrollTo(window.pageXOffset,e[0]),c=setTimeout(a,100)),i&2&&U(M,e[1]),(!F||i&2)&&O!==(O=e[1].toFixed(2)+"")&&le(ee,O),i&4&&U(A,e[2].numPoints),(!F||i&4)&&j!==(j=e[2].numPoints+"")&&le(te,j),i&4&&U(y,e[2].numSeries),(!F||i&4)&&Q!==(Q=e[2].numSeries+"")&&le(ie,Q),(!F||i&1)&&le(ae,e[0]),i&14){L=e[3];let r;for(r=0;r<L.length;r+=1){const W=Xe(e,L,r);n[r]?(n[r].p(W,i),Z(n[r],1)):(n[r]=Ye(W),n[r].c(),Z(n[r],1),n[r].m(w,null))}for(Re(),r=L.length;r<n.length;r+=1)ze(r);je()}},i(e){if(!F){for(let i=0;i<L.length;i+=1)Z(n[i]);F=!0}},o(e){n=n.filter(Boolean);for(let i=0;i<n.length;i+=1)ne(n[i]);F=!1},d(e){e&&l(f),e&&l(x),e&&l(p),e&&l(ue),e&&l(v),e&&l(se),e&&l(w),xe(n,e),Me=!1,et(Ie)}}}let ot=150;function mt(u,g,a){let c;tt(u,Te,B=>a(2,c=B));const f=[it,at,ut,st,nt,lt];let b=.02,q=.01;function s(){a(0,q=window.pageYOffset)}function S(){b=ye(this.value),a(1,b),a(0,q)}function k(){c.numPoints=ye(this.value),Te.set(c)}function V(){c.numSeries=ye(this.value),Te.set(c)}return u.$$.update=()=>{u.$$.dirty&1&&a(1,b=q>0?q>500?1:q/500:.01)},[q,b,c,f,s,S,k,V]}class ft extends He{constructor(g){super();Je(this,g,mt,rt,Ke,{})}}export{ft as default};
---
+++ _app/pages/gsap-mount-rushmore/index.svelte-4ff7e3c8.js
@@ -0,0 +1,61 @@
+import{S as qe,i as ze,s as Ee,e as b,t as S,c as w,a as _,g as A,d as o,f as F,D as t,E as de,k as P,n as T,G as Pe,P as Te,b as v,H as ye,I as Ce,h as X,M as Ue,Y as Fe,A as We,W as Ie,Z as he,O as Je,_ as Re,$ as ke,a0 as De,a1 as Ge,a2 as Oe,a3 as ee,a4 as be}from"../../chunks/vendor-279e1e6e.js";function we(a,s,c){const f=a.slice();return f[21]=s[c],f[23]=c,f}function _e(a){let s,c,f,p;return{c(){s=b("option"),c=S("shot "),f=S(a[23]),this.h()},l(i){s=w(i,"OPTION",{});var m=_(s);c=A(m,"shot "),f=A(m,a[23]),m.forEach(o),this.h()},h(){s.__value=p=a[21],s.value=s.__value},m(i,m){F(i,s,m),t(s,c),t(s,f)},p:de,d(i){i&&o(s)}}}function xe(a){let s,c,f,p,i,m,u,D,q,R,N,G,K,O,W,n,z,j,I,r,E,d,B,C,Q,U,x,te,H,ne,oe,L,ae,k,re,Z,V,Y,se,ie,ce,M=a[5],g=[];for(let e=0;e<M.length;e+=1)g[e]=_e(we(a,M,e));return{c(){s=b("p"),c=S(`Scrollpoints on a 3d model. Wait for the model to load, then do scrolly. Simple lerp of the
+    camera; better solution would be to incorporate rotation on the camera so we have better
+    maneouvering. Also needs slerp and easing.`),f=P(),p=b("select");for(let e=0;e<g.length;e+=1)g[e].c();i=P(),m=b("div"),u=b("pre"),D=S(a[2]),q=S(`
+From `),R=S(a[3]),N=S(" -> "),G=S(a[4]),K=S(" : "),O=S(a[0]),W=P(),n=b("div"),z=b("div"),j=P(),I=b("p"),r=S(`George Washington (February 22, 1732[b] \u2013 December 14, 1799) was an American political
+        leader, military general, statesman, and Founding Father who served as the first president
+        of the United States from 1789 to 1797. Appointed by the Continental Congress as commander
+        of the Continental Army, Washington led the Patriot forces to victory in the American
+        Revolutionary War, and presided at the Constitutional Convention of 1787, which established
+        the Constitution of the United States and a federal government. Washington has been called
+        the "Father of the Nation"[10] for his manifold leadership in the formative days of the
+        country.`),E=P(),d=b("div"),B=P(),C=b("p"),Q=S(`Thomas Jefferson (April 13, 1743[a] \u2013 July 4, 1826) was an American statesman, diplomat,
+        lawyer, architect, philosopher, and Founding Father who served as the third president of the
+        United States from 1801 to 1809. He had previously served as the second vice president of
+        the United States under John Adams between 1797 and 1801, and as the first United States
+        secretary of state under George Washington between 1790 and 1793. The principal author of
+        the Declaration of Independence, Jefferson was a proponent of democracy, republicanism, and
+        inpidual rights, motivating American colonists to break from the Kingdom of Great Britain
+        and form a new nation; he produced formative documents and decisions at both the state and
+        national levels.`),U=P(),x=b("div"),te=P(),H=b("p"),ne=S(`Theodore Roosevelt Jr. (/\u02C8ro\u028Az\u0259v\u025Blt/ ROH-z\u0259-velt;[b] October 27, 1858 \u2013 January 6, 1919),
+        often referred to as Teddy or his initials T. R., was an American politician, statesman,
+        conservationist, naturalist, historian, and writer who served as the 26th president of the
+        United States from 1901 to 1909. He previously served as the 25th vice president under
+        William McKinley from March to September 1901, and as the 33rd governor of New York from
+        1899 to 1900. Having assumed the presidency after McKinley's assassination, Roosevelt
+        emerged as a leader of the Republican Party and became a driving force for anti-trust and
+        Progressive policies.`),oe=P(),L=b("div"),ae=P(),k=b("p"),re=S(`Abraham Lincoln (/\u02C8l\u026A\u014Bk\u0259n/; February 12, 1809 \u2013 April 15, 1865) was an American lawyer and
+        statesman who served as the 16th president of the United States from 1861 until his
+        assassination in 1865. Lincoln led the nation through the American Civil War and succeeded
+        in preserving the Union, abolishing slavery, bolstering the federal government, and
+        modernizing the U.S. economy.`),Z=P(),V=b("div"),Y=b("p"),se=S("Other stuff here."),this.h()},l(e){s=w(e,"P",{});var l=_(s);c=A(l,`Scrollpoints on a 3d model. Wait for the model to load, then do scrolly. Simple lerp of the
+    camera; better solution would be to incorporate rotation on the camera so we have better
+    maneouvering. Also needs slerp and easing.`),l.forEach(o),f=T(e),p=w(e,"SELECT",{});var h=_(p);for(let le=0;le<g.length;le+=1)g[le].l(h);h.forEach(o),i=T(e),m=w(e,"DIV",{id:!0,class:!0});var $=_(m);u=w($,"PRE",{style:!0});var J=_(u);D=A(J,a[2]),q=A(J,`
+From `),R=A(J,a[3]),N=A(J," -> "),G=A(J,a[4]),K=A(J," : "),O=A(J,a[0]),J.forEach(o),$.forEach(o),W=T(e),n=w(e,"DIV",{id:!0,class:!0});var y=_(n);z=w(y,"DIV",{id:!0,class:!0}),_(z).forEach(o),j=T(y),I=w(y,"P",{id:!0});var fe=_(I);r=A(fe,`George Washington (February 22, 1732[b] \u2013 December 14, 1799) was an American political
+        leader, military general, statesman, and Founding Father who served as the first president
+        of the United States from 1789 to 1797. Appointed by the Continental Congress as commander
+        of the Continental Army, Washington led the Patriot forces to victory in the American
+        Revolutionary War, and presided at the Constitutional Convention of 1787, which established
+        the Constitution of the United States and a federal government. Washington has been called
+        the "Father of the Nation"[10] for his manifold leadership in the formative days of the
+        country.`),fe.forEach(o),E=T(y),d=w(y,"DIV",{id:!0,class:!0}),_(d).forEach(o),B=T(y),C=w(y,"P",{id:!0});var pe=_(C);Q=A(pe,`Thomas Jefferson (April 13, 1743[a] \u2013 July 4, 1826) was an American statesman, diplomat,
+        lawyer, architect, philosopher, and Founding Father who served as the third president of the
+        United States from 1801 to 1809. He had previously served as the second vice president of
+        the United States under John Adams between 1797 and 1801, and as the first United States
+        secretary of state under George Washington between 1790 and 1793. The principal author of
+        the Declaration of Independence, Jefferson was a proponent of democracy, republicanism, and
+        inpidual rights, motivating American colonists to break from the Kingdom of Great Britain
+        and form a new nation; he produced formative documents and decisions at both the state and
+        national levels.`),pe.forEach(o),U=T(y),x=w(y,"DIV",{id:!0,class:!0}),_(x).forEach(o),te=T(y),H=w(y,"P",{id:!0});var me=_(H);ne=A(me,`Theodore Roosevelt Jr. (/\u02C8ro\u028Az\u0259v\u025Blt/ ROH-z\u0259-velt;[b] October 27, 1858 \u2013 January 6, 1919),
+        often referred to as Teddy or his initials T. R., was an American politician, statesman,
+        conservationist, naturalist, historian, and writer who served as the 26th president of the
+        United States from 1901 to 1909. He previously served as the 25th vice president under
+        William McKinley from March to September 1901, and as the 33rd governor of New York from
+        1899 to 1900. Having assumed the presidency after McKinley's assassination, Roosevelt
+        emerged as a leader of the Republican Party and became a driving force for anti-trust and
+        Progressive policies.`),me.forEach(o),oe=T(y),L=w(y,"DIV",{id:!0,class:!0}),_(L).forEach(o),ae=T(y),k=w(y,"P",{id:!0,class:!0});var ue=_(k);re=A(ue,`Abraham Lincoln (/\u02C8l\u026A\u014Bk\u0259n/; February 12, 1809 \u2013 April 15, 1865) was an American lawyer and
+        statesman who served as the 16th president of the United States from 1861 until his
+        assassination in 1865. Lincoln led the nation through the American Civil War and succeeded
+        in preserving the Union, abolishing slavery, bolstering the federal government, and
+        modernizing the U.S. economy.`),ue.forEach(o),y.forEach(o),Z=T(e),V=w(e,"DIV",{});var ve=_(V);Y=w(ve,"P",{});var ge=_(Y);se=A(ge,"Other stuff here."),ge.forEach(o),ve.forEach(o),this.h()},h(){a[1]===void 0&&Pe(()=>a[6].call(p)),Te(u,"font-size","smallest"),v(m,"id","the-thing"),v(m,"class","svelte-1q4chqb"),v(z,"id","george-washington-transition"),v(z,"class","transition svelte-1q4chqb"),v(I,"id","george-washington"),v(d,"id","thomas-jefferson-transition"),v(d,"class","transition svelte-1q4chqb"),v(C,"id","thomas-jefferson"),v(x,"id","theodore-roosevelt-transition"),v(x,"class","transition svelte-1q4chqb"),v(H,"id","theodore-roosevelt"),v(L,"id","abraham-lincoln-transition"),v(L,"class","transition svelte-1q4chqb"),v(k,"id","abraham-lincoln"),v(k,"class","bottom svelte-1q4chqb"),v(n,"id","presidents"),v(n,"class","svelte-1q4chqb")},m(e,l){F(e,s,l),t(s,c),F(e,f,l),F(e,p,l);for(let h=0;h<g.length;h+=1)g[h].m(p,null);ye(p,a[1]),F(e,i,l),F(e,m,l),t(m,u),t(u,D),t(u,q),t(u,R),t(u,N),t(u,G),t(u,K),t(u,O),F(e,W,l),F(e,n,l),t(n,z),t(n,j),t(n,I),t(I,r),t(n,E),t(n,d),t(n,B),t(n,C),t(C,Q),t(n,U),t(n,x),t(n,te),t(n,H),t(H,ne),t(n,oe),t(n,L),t(n,ae),t(n,k),t(k,re),F(e,Z,l),F(e,V,l),t(V,Y),t(Y,se),ie||(ce=Ce(p,"change",a[6]),ie=!0)},p(e,[l]){if(l&32){M=e[5];let h;for(h=0;h<M.length;h+=1){const $=we(e,M,h);g[h]?g[h].p($,l):(g[h]=_e($),g[h].c(),g[h].m(p,null))}for(;h<g.length;h+=1)g[h].d(1);g.length=M.length}l&34&&ye(p,e[1]),l&4&&X(D,e[2]),l&8&&X(R,e[3]),l&16&&X(G,e[4]),l&1&&X(O,e[0])},i:de,o:de,d(e){e&&o(s),e&&o(f),e&&o(p),Ue(g,e),e&&o(i),e&&o(m),e&&o(W),e&&o(n),e&&o(Z),e&&o(V),ie=!1,ce()}}}const Se=1e3,Ae=500;function He(a,s,c){let f;const p=[];let i=new Fe(75,Se/Ae,.1,1e3),m,u=0;new be,new be;const D=["george-washington","thomas-jefferson","theodore-roosevelt","abraham-lincoln"],q=[{pos:{x:0,y:0,z:5},quat:{x:0,y:0,z:0,w:1}},{pos:{x:-151.44999999999962,y:151.64999999999998,z:-197.20000000000022},quat:{x:0,y:0,z:0,w:1}},{pos:{x:-25.149999999998887,y:166.75000000000003,z:-303.00000000000034},quat:{x:0,y:0,z:0,w:1}},{pos:{x:85.85000000000146,y:151.65,z:-409.00000000000057},quat:{x:0,y:0,z:0,w:1}},{pos:{x:156.45000000000218,y:111.14999999999999,z:-333.35000000000116},quat:{x:0,y:0,z:0,w:1}}],R=r=>{r&&(i.position.x=r.pos.x,i.position.y=r.pos.y,i.position.z=r.pos.z,i.quaternion.x=r.quat.x,i.quaternion.y=r.quat.y,i.quaternion.z=r.quat.z,i.quaternion.w=r.quat.w)},N=()=>{const r=document.getElementById("the-thing"),E=new Re,d=new ke;d.setSize(Se,Ae),r.appendChild(d.domElement),R(q[0]);const B=new De(16777215);E.add(B),new Ge().load("mount_rushmore/scene.gltf",U=>{console.log("loaded"),E.add(U.scene)},U=>{console.log(`${U.loaded} / ${U.total}`)},U=>{console.error(U)});const C=function(){new he,c(2,m=JSON.stringify(i.position)+`
+`+JSON.stringify(i.quaternion)+`
+`+JSON.stringify(q[z])),f||requestAnimationFrame(C),d.render(E,i)};f=setInterval(C,100),C()},G=()=>{f&&(console.log("clearing interval"),clearInterval(f))},K=()=>{Oe.exports.gsap.registerPlugin(ee.exports.ScrollTrigger),ee.exports.ScrollTrigger.defaults({start:"top top",end:"bottom top+=10%",scrub:!0,pin:!0}),D.forEach((r,E)=>{p.push(ee.exports.ScrollTrigger.create({trigger:`#${r}-transition`,pin:!1,markers:!1,end:"bottom top",onUpdate:d=>{d.isActive&&(c(3,n=d.direction===-1?E+1:E),c(4,z=d.direction===-1?E:E+1),c(0,u=d.direction===-1?1-d.progress:d.progress),console.log(`${n} -> ${z} ${u}`))}})),p.push(ee.exports.ScrollTrigger.create({start:"top top+=250px",trigger:`#${r}`,pin:!0}))})},O=()=>{p.forEach(r=>{r.kill()})};We(()=>{N(),K()}),Ie(()=>{G(),O()});let W,n=0,z=0;const j=r=>{if(r>=0&&r<=1){const E=new he(q[n].pos.x,q[n].pos.y,q[n].pos.z),d=new he(q[z].pos.x,q[z].pos.y,q[z].pos.z);i.position.lerpVectors(E,d,r)}};function I(){W=Je(this),c(1,W),c(5,q)}return a.$$.update=()=>{a.$$.dirty&1&&j(u),a.$$.dirty&2&&R(W)},[u,W,m,n,z,q,I]}class Ve extends qe{constructor(s){super();ze(this,s,He,xe,Ee,{})}}export{Ve as default};
